ea14d5c57e70bbd2a76770fbcfbb87ef5f43bcc6
yasuhiro_hirano <157764716+yasuhiro-h1988@users.noreply.github.com>
2024-01-26 10:11:02 +0900

    Add files via upload

---
+++ アプリ実行/appstart.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    /* â‘ÎƒpƒX */
+    char passbuf[] = "A.txt";
+
+    system(passbuf);
+
+    printf("I—¹‚µ‚Ü‚·\n");
+
+    return 0;
+}
